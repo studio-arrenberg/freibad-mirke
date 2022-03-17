cd0a8e9a39b9fa730322803f4c39fc61a1d61cb7
Moritz <41573605+moritzwindmann@users.noreply.github.com>
2022-03-17 20:26:53 +0100

    Revert "Footer Update"
    This reverts commit 960b5d029694160e1eba36da5c5151fcd263d712.

--- header.php
+++ header.php
@@ -78,7 +78,7 @@
 
 		<?php if ( is_front_page() ) { ?>
 			<!-- Start introduction -->
-			<!-- <div class="container mx-auto my-12 border-b pb-12">
+			<div class="container mx-auto my-12 border-b pb-12">
 				<h1 class="font-bold text-lg text-secondary uppercase">TailPress</h1>
 				<h2 class="text-3xl lg:text-7xl tracking-tight font-extrabold my-4">Rapidly build your WordPress theme
 					with <a href="https://tailwindcss.com" class="text-primary">Tailwind CSS</a>.</h2>
@@ -88,7 +88,7 @@
 				<a href="https://github.com/jeffreyvr/tailpress"
 					class="w-full sm:w-auto flex-none bg-gray-900 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200">View
 					on Github</a>
-			</div> -->
+			</div>
 			<!-- End introduction -->
 		<?php } ?>
 
